d9a6812b47aef144057e195ec1a5a0f6bd27109e
RedAvocado22 <minhcuong2922004@gmail.com>
2024-06-17 17:40:57 +0700

    remove some files

--- PRO192-main/Assignment/library-management/src/utils/exception/LibraryFullException.java
+++
@@ -1,8 +0,0 @@
-package utils.exception;
-
-public class LibraryFullException extends Exception {
-
-    public LibraryFullException(String message) {
-        super(message);
-    }
-}
--- PRO192_Meo/OrganizationManager/src/DTO/Role.java
+++
@@ -1,5 +0,0 @@
-package DTO;
-
-public interface Role {
-    public void createWorker();
-}
