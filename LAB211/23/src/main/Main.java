4d881bc67ec250b86141afcc64b0ddef65ccb937
RedAvocado <minhcuong2922004@gmail.com>
2024-10-05 14:55:08 +0700

    feat: create interface for program

--- LAB211/23/src/main/Main.java
+++ LAB211/23/src/main/Main.java
@@ -3,8 +3,14 @@
   Student name : Nguyen Minh Cuong
   Due date     :  
  */
-
 package main;
+
+import bo.FruitBO;
+import bo.OrderBO;
+import constant.IConstant;
+import mock.Data;
+import util.Helper;
+import util.Validate;
 
 /**
  *
@@ -16,7 +22,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("(Con me may)");
+        FruitBO fruitBO = new FruitBO(Data.fruits);
+        OrderBO orderBO = new OrderBO(Data.orders);
+
+        do {
+            Helper.menu();
+
+            int choice = Validate.getInt("Choice: ",
+                    IConstant.RANGE_MES,
+                    IConstant.INTEGER_MES,
+                    1, 4
+            );
+
+            switch (choice) {
+                case 1 -> {
+                    fruitBO.add();
+                }
+
+                case 2 -> {
+                    orderBO.display();
+                }
+
+                case 3 -> {
+                    if (!orderBO.add(fruitBO)) {
+                        System.out.println("Fruit is sold out!");
+                    }
+                }
+            }
+        } while (true);
     }
 
 }
