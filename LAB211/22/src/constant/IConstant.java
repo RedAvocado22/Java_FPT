01d5153fbc6ae9cc8c20434732bbfd59964be414
RedAvocado <minhcuong2922004@gmail.com>
2024-10-07 16:24:41 +0700

    feat: init p0023

---
+++ LAB211/22/src/constant/IConstant.java
@@ -0,0 +1,32 @@
+/*
+  Student ID   : HE187382
+  Student name : Nguyen Minh Cuong
+  Due date     :  
+ */
+package constant;
+
+/**
+ *
+ * @author Minh Cuong
+ */
+public interface IConstant {
+
+    String INTEGER_MES = "It must be an integer!";
+    String DOUBLE_MES = "It must be a double!";
+    String RANGE_MES = "Out of range!";
+    String STRING_MES = "Invalidated!";
+    String POSITIVE_MES = "It must be positive!";
+
+    String REGEX_NAME = "^[A-Za-z ]+$";
+    String REGEX_ID = "^[A-Za-z0-9 ]+$";
+    String REGEX_ADDRESS = "^[A-Za-z0-9 ]+$";
+
+    int MIN_AGE = 18;
+    int MAX_AGE = 50;
+
+    String REGEX_YES_NO = "^[yYnN]$";
+    String YES = "y";
+
+    String UP = "UP";
+    String DOWN = "DOWN";
+}
