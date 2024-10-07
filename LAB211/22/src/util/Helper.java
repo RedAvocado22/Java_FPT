300bc6fd6e7c95a03cbc91feef34308337a26098
RedAvocado <minhcuong2922004@gmail.com>
2024-10-07 16:26:01 +0700

    feat: add function for helper

---
+++ LAB211/22/src/util/Helper.java
@@ -0,0 +1,53 @@
+/*
+  Student ID   : HE187382
+  Student name : Nguyen Minh Cuong
+  Due date     :  
+ */
+package util;
+
+import constant.IConstant;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Minh Cuong
+ */
+public class Helper {
+
+    private Helper() {
+    }
+
+    /**
+     * get the current year as an integer.
+     *
+     * @return the current year as an integer
+     */
+    public static int getCurrentYear() {
+        return Calendar.getInstance().get(Calendar.YEAR);
+    } 
+    
+    /**
+     * Display menu for user to choose.
+     */
+    public static void menu() {
+        System.out.println("\n========= Fruit Shopping =========");
+        System.out.println("1. Add fruit");
+        System.out.println("2. View orders");
+        System.out.println("3. Shopping (for buyer)");
+        System.out.println("5. Exit");
+    }
+
+    /**
+     * Check if user's want to continue or not
+     *
+     * @return true if input = Y, false if not
+     */
+    public static boolean isContinue() {
+        String respond = Validate.getString(
+                "Do you want to continue? ",
+                "Only choose Y/N",
+                IConstant.REGEX_YES_NO);
+        return respond.equalsIgnoreCase(IConstant.YES);
+    }
+
+}
