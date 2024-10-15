36cf3069e48c4d0770c13c969619e93b1163f049
RedAvocado <minhcuong2922004@gmail.com>
2024-10-15 09:17:41 +0700

    feat: add constant

--- LAB211/22/src/constant/IConstant.java
+++ LAB211/22/src/constant/IConstant.java
@@ -14,19 +14,27 @@ public interface IConstant {
     String INTEGER_MES = "It must be an integer!";
     String DOUBLE_MES = "It must be a double!";
     String RANGE_MES = "Out of range!";
-    String STRING_MES = "Invalidated!";
+    String STRING_MES = "Invalidated string!";
     String POSITIVE_MES = "It must be positive!";
 
     String REGEX_NAME = "^[A-Za-z ]+$";
     String REGEX_ID = "^[A-Za-z0-9 ]+$";
     String REGEX_ADDRESS = "^[A-Za-z0-9 ]+$";
-
-    int MIN_AGE = 18;
-    int MAX_AGE = 50;
-
+    String REGEX_PHONE_NUMBER = "^[0-9]{10,}$";
+    String REGEX_EMAIL = "^[\\w]+@[\\w.]+\\.[A-Za-z]{2,}$";
+	String REGEX_TEXT = "^[A-Za-z0-9 ,\\.]+$";
+    
+    int MIN_YEAR = 1900;
+	int EXPERIENCE_TYPE = 0;
+	int FRESHER_TYPE = 1;
+	int INTERN_TYPE = 2;
+	int MIN_SEMESTER = 1;
+	int MAX_SEMESTER = 9;
+    
     String REGEX_YES_NO = "^[yYnN]$";
     String YES = "y";
 
-    String UP = "UP";
-    String DOWN = "DOWN";
+	String SEARCH_NAME_ONLY = "Search name only";
+	String SEARCH_NAME_AND_TYPE = "Search name and type";
+	String DISPLAY_NAME_ONLY = "Display name only";
 }
